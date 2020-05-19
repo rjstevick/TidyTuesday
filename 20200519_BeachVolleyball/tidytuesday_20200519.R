1f0661f14a72bf91e5003fcb76aad61846cd05b9
rjstevick <rstevick@my.uri.edu>
2020-05-19 21:59:13 +0200

    week 21 beach volleyball

---
+++ 20200519_BeachVolleyball/tidytuesday_20200519.R
@@ -0,0 +1,57 @@
+# Beach volleyball
+# TidyTuesday 2020 week 21
+# RJS updated 5/19/2020
+
+# Load libraries ---------------------
+
+library(tidyverse)
+library(ggalt)
+library(ggtext)
+library(patchwork)
+
+sessionInfo()
+theme_set(theme_light())
+
+# Load data --------------------------
+
+vb_matches <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-19/vb_matches.csv', guess_max = 76000)
+
+
+# Data formatting & analysis ---------
+
+vb_locations<- vb_matches %>%
+  gather(c(w_p1_country, w_p2_country, l_p1_country, l_p2_country),
+         key="player", value="player_country") %>%
+  group_by(country, player_country)  %>%
+  count() %>% drop_na() %>% ungroup()
+
+# define scale so the outlier isn't too obnoxious
+vb_locations$ncolors <- cut(vb_locations$n, 
+                            breaks=c(0,10,25,50,75,100,125,150,175,200,1000,25000,50000,
+                                     max(vb_locations$n)),
+                            labels=c(10,25,50,75,100,125,150,175,200,1000,25000,50000,
+                                     max(vb_locations$n)))
+
+# Plotting ---------------------------
+
+ggplot(vb_locations, 
+       aes(y=reorder(country,-n), 
+           x=reorder(player_country,-n),
+           size=ncolors, color=ncolors))+
+  geom_point(alpha=0.8)+
+  scale_colour_viridis_d(option = "plasma") +
+  theme(axis.text.x=element_text(angle=90, hjust=1),
+        axis.text = element_text(size=8),
+        legend.position="right",
+        plot.background = element_rect(fill="bisque"),
+        legend.background = element_blank(),panel.background = element_blank(),legend.key=element_blank())+
+  labs(x="Player countries", y="Match location", 
+       color="Number of \ncombinations",size="Number of \ncombinations",
+       caption = "Plot by @rjstevick \n Source: BigTimeStats",
+       title = "Where do beach volleyball players come from and where do they play?",
+       subtitle = "The most common combo is from the USA, playing in the USA")
+
+  
+# Saving -----------------------------
+
+ggsave("BeachVolleyball_plot.png", bg="transparent", width = 12, height = 7, dpi=400)
