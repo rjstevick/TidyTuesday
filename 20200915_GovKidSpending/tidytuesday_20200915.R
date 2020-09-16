a89aa0cbba21da0a4dc1c9578e90a1e79630c5b7
Rebecca Stevick <rjstevick@gmail.com>
2020-09-16 15:35:44 +0200

    week 38 edits

--- 20200915_GovKidSpending/tidytuesday_20200915.R
+++ 20200915_GovKidSpending/tidytuesday_20200915.R
@@ -1,31 +1,34 @@
 # Government spending on kids
 # TidyTuesday 2020 week 38
-# Rebecca Stevick updated 9/15/2020
+# Rebecca Stevick updated 9/16/2020
 
 # Load libraries ---------------
 library(tidyverse)
 library(gganimate)
-
 
 # Load data --------------------
 tuesdata <- tidytuesdayR::tt_load('2020-09-15')
 kids <- tuesdata$kids
 
 # Analysis and plotting ------
-
 basemap <- kids %>% 
+  # select only PK12ed and join with the map data per state
   mutate(region=tolower(state)) %>% 
   filter(variable=="PK12ed") %>% 
   left_join(map_data("state")) %>% 
   # make map area
   ggplot(aes(x = long, y = lat, group = group, fill = inf_adj_perchild)) + 
-  # add world map
   geom_polygon(color = "white") +
   # change color scheme
-  scale_fill_viridis_c(name = "Amount per child in $USD \n(adjusted for inflation)",
-                       labels = scales::dollar_format()) +
+  scale_fill_viridis_b(name = "Amount per child in $USD \n(adjusted for inflation)",
+                       labels = scales::dollar_format(), breaks = c(0,4,8,12,16,20), limits=c(0,20)) +
   # remove all theme elements and fix the x-y so the map doesn't warp
-  theme_void() + coord_fixed(1.3) + theme(legend.position = "bottom")
+  theme_void() + coord_fixed(1.3) + 
+  guides(fill = guide_colourbar(title.position="top")) +
+  theme(plot.background = element_rect(fill="black"),plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"),
+        legend.position = c(.15,.1), legend.direction = "horizontal", text = element_text(family="Tahoma", color="white"),
+        legend.title = element_text(size=8),
+        plot.title = element_text(family="Education Pencil", size=15)) # font from https://fr.ffonts.net/Education-Pencil.font.download
 
 #animate based on year
 plotanimate<-basemap + transition_manual(frames = year) +
@@ -36,7 +39,7 @@ plotanimate<-basemap + transition_manual(frames = year) +
 
 # render animation
 animate(plot = plotanimate, nframes = length(unique(kids$year)), 
-        fps = 4, end_pause = 8, height = 450, width = 740, res = 100)
+        fps = 4, end_pause = 8, height = 450, width = 670, res = 100)
 
 # Saving -----------------------
 anim_save("GovKidSpending_plot.gif")
