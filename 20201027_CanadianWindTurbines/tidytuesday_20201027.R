f6c90004cea56cfe36a4cf0c5b31d717d723360f
rjstevick <rstevick@my.uri.edu>
2020-11-01 11:32:48 +0100

    week 44 windmill edits

--- 20201027_CanadianWindTurbines/tidytuesday_20201027.R
+++ 20201027_CanadianWindTurbines/tidytuesday_20201027.R
@@ -1,6 +1,6 @@
 # Canadian Wind Turbines
 # TidyTuesday 2020 week 44
-# Rebecca Stevick updated 10/27/2020
+# Rebecca Stevick updated 11/1/2020
 
 # Load libraries -----------------
 library(tidyverse)
@@ -11,32 +11,45 @@ library(extrafont)
 tuesdata <- tidytuesdayR::tt_load('2020-10-27')
 
 # Analysis and plotting ----------
-
 tuesdata$`wind-turbine` %>% 
-  group_by(province_territory) %>% 
-  summarise(count=n(), 
-            meanrad=mean(rotor_diameter_m), 
-            meanlat=mean(latitude), 
-            meanlong=mean(longitude)) %>% 
+  # group the data by province/territory to calculate summary statistics
+  group_by(province_territory) %>%
+  # now summarize the data for count and mean of variables
+  summarise(count=n(), # count the number of windmills in each province
+            meandiam=mean(rotor_diameter_m), # mean of their diameters
+            meanlat=mean(latitude), # mean of their latitudes
+            meanlong=mean(longitude)) %>% # mean of their longitudes
+  # start to plot
   ggplot()+
+  # add a map of Canada
   geom_polygon(data=map_data("world") %>% filter(region=="Canada"), 
                aes(x=long, y=lat, group=group), fill="grey90", color="white") +
+  # add a point and star at each mean windmill location
   geom_point(aes(x=meanlong, y=meanlat, color=count), size=4, shape=20)+
-  geom_point(aes(x=meanlong, y=meanlat, size=meanrad, color=count), shape=8)+
-  geom_text_repel(aes(x=meanlong, y=meanlat, color=count, label=str_wrap(province_territory, 10)),
-            nudge_y=1.5, family="Amaranth", segment.alpha=0)+
+  geom_point(aes(x=meanlong, y=meanlat, size=meandiam, color=count), shape=8)+
+  # add labels for the points
+  geom_text_repel(aes(x=meanlong, y=meanlat, color=count, 
+                      label=str_wrap(province_territory, 14)), # wrap the labels so they fit better
+                      nudge_y=1.5, family="Amaranth", segment.alpha=0)+
+  # change the color scheme
+  scale_color_gradient(low="red3",high="black")+
+  # add overall theme and fix the coordinates so the map doesn't warp
+  theme_void() + coord_fixed(1.3)+
+  # edit the theme and legend position
+  theme(text=element_text(family="Amaranth"),
+        legend.position=c(0.9,0.7), legend.box="horizontal", legend.direction = "vertical")+
+  # add title to the top left of the plot
   geom_text(aes(x=-110, y=78, label="Canadian Windmills by Province/Territory"),
             size=7, fontface="bold", family="Amaranth")+
-  geom_text(aes(x=-110, y=76, 
-                label="Each point represents the mean location, size, and radius of the windmills in the territory."),
-            size=4, family="Amaranth", color="red3")+
-  scale_color_gradient(low="red3",high="black")+
-  theme_void() + coord_fixed(1.3)+
-  theme(text=element_text(family="Amaranth"),
-        legend.position=c(0.85,0.8), legend.box="horizontal", legend.direction = "vertical")+
+  # add subtitle under the title
+  geom_text(aes(x=-110, y=76, label="Each point represents the mean location, size, and diameter of the windmills in the territory."),
+            size=4, family="Amaranth", color="red4")+
+  # add caption on bottom left inset
+  geom_text(aes(x=-125, y=43, label="data from open.canada.ca | plot by @rjstevick for #TidyTuesday"),
+            size=3, family="Amaranth", color="grey60")+
   # add those labels
-  labs(color="Number of \nWindmills", size="Mean radius \nof Windmills",
-       caption="data from open.canada.ca | plot by @rjstevick for #TidyTuesday")
+  labs(color="Number of \nWindmills", 
+       size="Mean diameter \nof Windmills (m)")
 
 # Saving -------------------------
 ggsave("CanadianWindTurbines_plot.png", bg="transparent", width = 11, height = 7, dpi = 400)
