737f0c1df93ba7c03c72facb61b57c01f020ce6c
rjstevick <rstevick@my.uri.edu>
2020-12-16 11:46:20 -0500

    week 50 edits

--- 20201208_Women2020/tidytuesday_20201208.R
+++ 20201208_Women2020/tidytuesday_20201208.R
@@ -1,6 +1,6 @@
 # Women of 2020 poster
 # TidyTuesday 2020 week 50
-# Rebecca Stevick updated 12/08/2020
+# Rebecca Stevick updated 12/16/2020
 
 # Load libraries -----------------
 library(tidyverse)
@@ -14,7 +14,7 @@ tuesdata$women %>%
   # make a  grid guide for the points, based on 8 rows/columns
   mutate(num = row_number(), x_pos = (num - 1) %/% 10, y_pos = 10 - (num - 1) %% 10 - 1) %>%
   # time to plot
-  ggplot()+ 
+  ggplot()+
   # add background rectangles based on woman's category
   geom_rect(aes(xmin = x_pos-0.5, xmax = x_pos+0.5, ymin = y_pos-0.5, ymax = y_pos+0.5, fill=category), color="#FBEAD6", lwd=2) +
   # add photo of woman
@@ -23,7 +23,7 @@ tuesdata$women %>%
   geom_text(aes(x = x_pos, y = y_pos-0.3, label = str_wrap(name, width=15)), size = 4, hjust=0.5, lineheight=0.8) +
   # make sure the photos don't warp and remove extra white space
   scale_size_identity() + scale_x_continuous(expand=c(0,0)) + scale_y_continuous(expand=c(0,0)) +
-  # change background colors
+  # change background colors and make glyphs bigger
   scale_fill_manual(values=c("#FBEAD6", "#83CDC0", "#D3A3A1", "#A79CA5","#466D53"),
                     guide = guide_legend(keyheight = unit(12, units = "mm"), keywidth = unit(12, units = "mm"))) +
   # change theme
@@ -32,8 +32,7 @@ tuesdata$women %>%
   theme(text = element_text(family="Amaranth", size=22), plot.title = element_text(size=40, hjust=0.5),
         legend.position = "bottom", legend.justification = c(0,0)) +
   # add those labels
-  labs(title="Women of 2020", fill=NULL, x=NULL, y=NULL,
-       caption = "data from the BBC | plot by @rjstevick for #TidyTuesday")
+  labs(title="Women of 2020", fill=NULL, caption = "data from the BBC | plot by @rjstevick for #TidyTuesday")
 
 # Saving -------------------------
 ggsave("Women2020_plot.png", bg = "transparent", width = 15, height = 15, dpi = 400)
