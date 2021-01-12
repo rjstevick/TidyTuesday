aa21ce90df542e809529a0ac67a639d93068184b
Rebecca Stevick <rjstevick@gmail.com>
2021-01-12 17:10:48 +0100

    week 3 code draft

--- 2021/20210105_TransitCostProject/tidytuesday_20210105.R
+++ 2021/20210105_TransitCostProject/tidytuesday_20210105.R
@@ -1,6 +1,6 @@
 # Transit cost project
 # TidyTuesday 2021 week 2
-# Rebecca Stevick updated 1/5/2021
+# Rebecca Stevick updated 1/12/2021
 
 # Load libraries -----------------
 library(tidyverse)
@@ -11,16 +11,26 @@ tuesdata <- tidytuesdayR::tt_load('2021-01-05')
 # Analysis and plotting ----------
 tuesdata$transit_cost %>%
   drop_na(country) %>%
+  # select the top 10 longest tunnel projects
   arrange(desc(tunnel)) %>% head(n = 10L) %>%
+  # start plotting
   ggplot(aes(x=tunnel, y=reorder(city,tunnel), fill=country))+
+  # add bar plot
   geom_col(alpha = 0.8)+
+  # add construction year to the end of each bar
   geom_text(aes(x=tunnel-10, label = year), size = 7, color="white", family="Baloo")+
+  # change the color of the bar
   scale_fill_manual(values = PNWColors::pnw_palette("Starfish", n=8))+
+  # remove the extra white space in the x-axis
   scale_x_continuous(expand = c(0,0))+
+  # change the global theme
   theme_minimal()+
-  theme(text=element_text(family="Baloo"),
-        plot.title = element_text(hjust=0, size=24), plot.caption = element_text(color="grey40"),
-        legend.position = "none", axis.text.y = element_text(size = 14))+
+  # edit the theme
+  theme(text=element_text(family="Baloo"), # change font
+        plot.title = element_text(hjust=0, size=24), # change position and size of title
+        plot.caption = element_text(color="grey40"), # change color of the caption
+        legend.position = "none", # remove the legend
+        axis.text.y = element_text(size = 14))+ # make the y-axis text bigger
   # add those labels
   labs(title="Longest transit tunnels in the world",
        x="Tunnel Length (km)", y=NULL,
--- 2021/20210112_ArtCollections/tidytuesday_20210112.R
+++ 2021/20210112_ArtCollections/tidytuesday_20210112.R
@@ -7,10 +7,24 @@ library(tidyverse)
 
 # Load data ----------------------
 tuesdata <- tidytuesdayR::tt_load('2021-01-12')
+artwork <- tuesdata$artwork
+artists <- tuesdata$artists
 
 # Analysis and plotting ----------
 tuesdata$artwork %>%
+  group_by(year) %>% summarise(meanheight=mean(height), meanwidth=mean(width)) %>% 
+  ggplot()+
+  geom_rect(aes(xmin=0, xmax=meanwidth, ymin=0, ymax=meanheight, fill=year), 
+            color="white", alpha=0.6)+
+  theme_minimal()
 
+tuesdata$artwork %>%
+  group_by(year) %>% summarise(meanheight=mean(height), meanwidth=mean(width)) %>% 
+  mutate(meanarea=meanheight*meanwidth) %>% 
+  ggplot(aes(x=year, y=meanarea))+
+  geom_line()+
+  theme_minimal()
+  
   # add those labels
   labs(
        caption = "data from Tate Art Museum | plot by @rjstevick for #TidyTuesday")
