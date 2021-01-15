b87808fa783ed1b5c8b6e937f5a4108c8adb2984
rjstevick <rstevick@my.uri.edu>
2021-01-15 20:40:19 +0100

    week 3 tate art plot

--- 2021/20210112_ArtCollections/tidytuesday_20210112.R
+++ 2021/20210112_ArtCollections/tidytuesday_20210112.R
@@ -1,33 +1,34 @@
 # Art Collections
 # TidyTuesday 2021 week 3
-# Rebecca Stevick updated 1/12/2021
+# Rebecca Stevick updated 1/15/2021
 
 # Load libraries -----------------
 library(tidyverse)
+library(ggstream)
+library(hrbrthemes)
+library(nationalparkcolors)
 
 # Load data ----------------------
 tuesdata <- tidytuesdayR::tt_load('2021-01-12')
-artwork <- tuesdata$artwork
-artists <- tuesdata$artists
 
 # Analysis and plotting ----------
 tuesdata$artwork %>%
-  group_by(year) %>% summarise(meanheight=mean(height), meanwidth=mean(width)) %>% 
-  ggplot()+
-  geom_rect(aes(xmin=0, xmax=meanwidth, ymin=0, ymax=meanheight, fill=year), 
-            color="white", alpha=0.6)+
-  theme_minimal()
-
-tuesdata$artwork %>%
-  group_by(year) %>% summarise(meanheight=mean(height), meanwidth=mean(width)) %>% 
-  mutate(meanarea=meanheight*meanwidth) %>% 
-  ggplot(aes(x=year, y=meanarea))+
-  geom_line()+
-  theme_minimal()
-  
+  filter(year >= 1740) %>%  drop_na(medium) %>% 
+  mutate(mediumother = fct_lump_n(medium, 15)) %>% 
+  group_by(year, mediumother) %>% count() %>% 
+  ggplot(aes(x = year, y = n, fill = reorder(mediumother,n))) +
+  geom_stream(bw = 0.4, color = "white", alpha = 0.7) +
+  scale_fill_manual(values = c(park_palette("Redwoods"), park_palette("GeneralGrant"), park_palette("CraterLake")))+
+  theme_ft_rc() +
+  theme(legend.position = c(0.72,0.88), legend.direction = "horizontal",
+        legend.text = element_text(size=8), plot.title = element_text(size=32),
+        panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank())+
   # add those labels
-  labs(
+  labs(title = "The Art of Embracing Change",
+       subtitle = "Evolution of art styles in the Tate Collection since the 1700s", 
+       fill = NULL, x = NULL, y = "Total number of works of art",
        caption = "data from Tate Art Museum | plot by @rjstevick for #TidyTuesday")
 
 # Saving -------------------------
-ggsave("ArtCollections_plot.png", bg = "transparent", width = 12, height = 5, dpi = 400)
+ggsave("ArtCollections_plot.png", width = 13, height = 7.5, dpi = 400)
+
