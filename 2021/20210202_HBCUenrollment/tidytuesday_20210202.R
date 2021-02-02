f13e7a0632c0e6d3886e6598aeb58fc5ab4b805f
Rebecca Stevick <rjstevick@gmail.com>
2021-02-02 18:17:26 +0100

    week 6 initial code

--- 2021/20210202_HBCUenrollment/tidytuesday_20210202.R
+++ 2021/20210202_HBCUenrollment/tidytuesday_20210202.R
@@ -7,9 +7,21 @@ library(tidyverse)
 
 # Load data ----------------------
 tuesdata <- tidytuesdayR::tt_load('2021-02-02')
+hs_students <- tuesdata$hs_students
+hbcu_all <- tuesdata$hbcu_all
 
 # Analysis and plotting ----------
- %>%
+hs_students %>%
+  filter(Total < 2020) %>% 
+  select(-contains("Error")) %>% 
+  pivot_longer(White1:`Two or more race`) %>% 
+  mutate(value=as.numeric(value)) %>% 
+  filter(name!="Total - Asian/Pacific Islander") %>% 
+  ggplot(aes(Total))+
+  geom_col(aes(y=value, fill=name), position="dodge")+
+  geom_point(aes(y=`Total, percent of all persons age 25 and over`))+
+  geom_line(aes(y=`Total, percent of all persons age 25 and over`))+
+  scale_y_continuous(labels=scales::percent_format(scale=1), limits=c(0,100))
 
 
   # add those labels
