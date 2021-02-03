b12f02318d9b09c0d5df31de198e29f034a89d2d
Rebecca Stevick <rjstevick@gmail.com>
2021-02-03 11:19:03 +0100

    week 6 hbcu education plot

--- 2021/20210126_PlasticPollution/tidytuesday_20210126.R
+++ 2021/20210126_PlasticPollution/tidytuesday_20210126.R
@@ -1,6 +1,6 @@
 # Plastic Pollution
 # TidyTuesday 2021 week 5
-# Rebecca Stevick updated 1/26/2021
+# Rebecca Stevick updated 2/2/2021
 
 # Load libraries -----------------
 library(tidyverse)
@@ -9,14 +9,14 @@ library(tidyverse)
 tuesdata <- tidytuesdayR::tt_load('2021-01-26')
 
 # Analysis and plotting ----------
-tuesdata$plastics %>% 
-  filter(country=="United States of America" & year==2019) %>%  
-  group_by(parent_company) %>% summarise_at(vars(hdpe:grand_total), sum, na.rm=TRUE) %>% 
-  pivot_longer(hdpe:pvc, names_to="plastic_type", values_to="count") %>% 
-  filter(parent_company!="Grand Total" & parent_company!="Unbranded") %>% 
-  filter(grand_total>10) %>% 
+tuesdata$plastics %>%
+  filter(country=="United States of America" & year==2019) %>%
+  group_by(parent_company) %>% summarise_at(vars(hdpe:grand_total), sum, na.rm=TRUE) %>%
+  pivot_longer(hdpe:pvc, names_to="plastic_type", values_to="count") %>%
+  filter(parent_company!="Grand Total" & parent_company!="Unbranded") %>%
+  filter(grand_total>10) %>%
   # remove plastic types that don't exist in this subset of data
-  filter(count!=0) %>% 
+  filter(count!=0) %>%
   # start plotting
   ggplot(aes(x=count, y=reorder(parent_company,-grand_total), fill=plastic_type))+
   geom_col()+
@@ -28,7 +28,7 @@ tuesdata$plastics %>%
   theme(text = element_text(size = 16, family = "Cochin"), legend.position = c(0.75,0.7),
         plot.title = element_text(size = 28, family = "Copperplate"), plot.subtitle = element_text(size=12))+
   # add those labels
-  labs(title = "All the polys", 
+  labs(title = "All the polys",
        subtitle = "Most abundant plastic pollution types and sources in the US in 2019",
        x = NULL, y = NULL, fill = "Plastic Type",
        caption = "data from Break Free from Plastic | plot by @rjstevick for #TidyTuesday")
--- 2021/20210202_HBCUenrollment/tidytuesday_20210202.R
+++ 2021/20210202_HBCUenrollment/tidytuesday_20210202.R
@@ -1,31 +1,51 @@
 # HBCU enrollment
 # TidyTuesday 2021 week 6
-# Rebecca Stevick updated 2/2/2021
+# Rebecca Stevick updated 2/3/2021
 
 # Load libraries -----------------
 library(tidyverse)
+library(hrbrthemes)
 
 # Load data ----------------------
 tuesdata <- tidytuesdayR::tt_load('2021-02-02')
 hs_students <- tuesdata$hs_students
-hbcu_all <- tuesdata$hbcu_all
 
 # Analysis and plotting ----------
 hs_students %>%
-  filter(Total < 2020) %>% 
-  select(-contains("Error")) %>% 
-  pivot_longer(White1:`Two or more race`) %>% 
-  mutate(value=as.numeric(value)) %>% 
-  filter(name!="Total - Asian/Pacific Islander") %>% 
-  ggplot(aes(Total))+
-  geom_col(aes(y=value, fill=name), position="dodge")+
-  geom_point(aes(y=`Total, percent of all persons age 25 and over`))+
-  geom_line(aes(y=`Total, percent of all persons age 25 and over`))+
-  scale_y_continuous(labels=scales::percent_format(scale=1), limits=c(0,100))
-
-
-  # add those labels
-  labs(caption = "data from Data.World | plot by @rjstevick for #TidyTuesday")
+  # remove random erroneous data in the Total column (which is actually year)
+  filter(Total < 2020) %>%
+  # remove the columns with standard error
+  select(-contains("Error")) %>%
+  # put all percentages by race/ethnicity into a single column
+  pivot_longer(White1:`Two or more race`) %>%
+  # remove this total category
+  filter(name != "Total - Asian/Pacific Islander") %>%
+  # add the education percentage as a numeric value
+  mutate(percent = as.numeric(value)) %>%
+  # define order of race/ethnicity variable
+  mutate(name = factor(name, levels=c("White1", "Asian/Pacific Islander - Pacific Islander",
+                                      "Two or more race", "Asian/Pacific Islander - Asian", "Black1",
+                                      "American Indian/\r\nAlaska Native", "Hispanic"))) %>%
+  # start plotting - year on the x-axis, education rate on the y-axis, color by race/ethnicity
+  ggplot(aes(x = Total, y = percent, fill = name))+
+  # add area with dodged position so they all plot in front of each other
+  geom_area(position = position_dodge(width = 0), alpha = 0.7, color = "white")+
+  # add color scheme and better labels for the fill colors
+  scale_fill_viridis_d(option = "A", labels = c("White", "Pacific Islander", "Two or more races",
+                                                "Asian", "Black","American Indian/Alaska Native", "Hispanic"))+
+  # add percent scale to the y-axis
+  scale_y_continuous(labels = scales::percent_format(scale = 1), limits = c(0,100))+
+  # add global theme
+  theme_ft_rc()+
+  # edit theme
+  theme(legend.position = c(0.2,0.73), legend.text = element_text(color="gray90"),
+        plot.caption = element_text(color="white"), plot.title = element_text(size = 26, family = "Education Pencil"),
+        panel.grid.minor.y = element_blank(), panel.grid.major.y = element_line(color="gray70"))+
+   # add those labels
+  labs(title = "The race for education",
+       subtitle = "Inequities in high school education rate have barely improved since 1940",
+       x = NULL, y = "Percent of persons age 25 and over with a high school degree", fill = NULL,
+       caption = "data from Data.World | plot by @rjstevick for #TidyTuesday")
 
 # Saving -------------------------
-ggsave("HBCUenrollment_plot.png", bg = "transparent", width = 15, height = 8, dpi = 400)
+ggsave("HBCUenrollment_plot.png", bg = "transparent", width = 10, height = 6, dpi = 400)
