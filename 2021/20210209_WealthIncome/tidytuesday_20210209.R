4fdf2ab066eea33c5c7b5fa54393613746460cd9
Rebecca Stevick <rjstevick@gmail.com>
2021-02-09 11:03:26 +0100

    add comments week 7 script

--- 2021/20210209_WealthIncome/tidytuesday_20210209.R
+++ 2021/20210209_WealthIncome/tidytuesday_20210209.R
@@ -1,4 +1,4 @@
-# Wealth and income
+# Wealth and income - lifetime earnings by race/gender
 # TidyTuesday 2021 week 7
 # Rebecca Stevick updated 2/9/2021
 
@@ -10,20 +10,30 @@ library(hrbrthemes)
 tuesdata <- tidytuesdayR::tt_load('2021-02-09')
 # use lifetime earn data, but many more included in tuesdata
 lifetime_earn <- tuesdata$lifetime_earn
+# recreate a version of figure 5 shown here https://apps.urban.org/features/wealth-inequality-charts/
 
 # Analysis and plotting ----------
 lifetime_earn %>%
+  # add a column with the fomatted earning values
   mutate(lifetime_earn_format = paste("$",round(lifetime_earn/1000000,1)," million", sep = ""),
+         # edit the hispanic category name
          race = recode(race, "Hispanic any race"="Hispanic")) %>%
   # start plotting
   ggplot(aes(x = race, y = lifetime_earn, fill = gender))+
+  # add barplot, with bars dodged by gender
   geom_col(position = position_dodge(width = 0.95))+
+  # annotate the top of each bar with its value
   geom_text(aes(label = lifetime_earn_format, y = lifetime_earn+100000), 
             position = position_dodge(width = 0.95), family = "Arial Narrow", color = "grey40")+
+  # add a horizontal line at y=0
   geom_abline()+
+  # add dollar signs to the y-axis scale
   scale_y_continuous(labels = scales::label_dollar())+
+  # edit the fill colors
   scale_fill_manual(values = c("midnightblue","plum4"))+
+  # change the global theme
   theme_ipsum()+
+  # edit the theme
   theme(panel.grid.major.x = element_blank(), axis.text.x = element_text(face = "bold", size = 16, color = "black"),
         legend.position = c(0.15,0.93), legend.direction = "horizontal", legend.text = element_text(size = 14, face = "bold"),
         plot.caption = element_text(color = "grey40"), axis.text.y = element_text(color = "grey40"),
