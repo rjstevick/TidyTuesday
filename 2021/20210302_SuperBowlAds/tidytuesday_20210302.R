39befc684f6e336190c0db11106f726515bb75bf
Rebecca Stevick <rjstevick@gmail.com>
2021-03-13 11:48:30 +0100

    superb owl heatmap week 10

--- 2021/20210302_SuperBowlAds/tidytuesday_20210302.R
+++ 2021/20210302_SuperBowlAds/tidytuesday_20210302.R
@@ -1,42 +1,39 @@
 # Superb Owl Ads
 # TidyTuesday 2021 week 10
-# Rebecca Stevick updated 3/2/2021
+# Rebecca Stevick updated 3/13/2021
 
 # Load libraries -----------------
 library(tidyverse)
 library(hrbrthemes)
-library(reshape2)
 
 # Load data ----------------------
 tuesdata <- tidytuesdayR::tt_load('2021-03-02')
 youtube <- tuesdata$youtube
 
 # Analysis and plotting ----------
-youtube %>%
-  group_by(funny, show_product_quickly, patriotic, celebrity, danger, animals, use_sex) %>%
-  count() %>% ungroup() %>% 
-  mutate(funny = replace(funny, funny==TRUE, "funny"),
-         show_product_quickly = replace(show_product_quickly, show_product_quickly==TRUE, "show_product_quickly"),
-         patriotic = replace(patriotic, patriotic==TRUE, "patriotic"),
-         celebrity = replace(celebrity, celebrity==TRUE, "celebrity"),
-         danger = replace(danger, danger==TRUE, "danger"),
-         animals = replace(animals, animals==TRUE, "animals"),
-         use_sex = replace(use_sex, use_sex==TRUE, "use_sex")) %>% 
-  na_if(FALSE) %>% 
-#  add_column(row=1:64) %>% 
-  unite("tactic", funny:use_sex, remove = FALSE) %>% 
-  melt(id.vars=c("funny", "show_product_quickly", "patriotic", "celebrity", "danger", "animals", "use_sex")) -> hey
-  
-  
-  
-  pivot_longer(funny:use_sex) %>% 
-  ggplot(aes(x=tactic, y=row, size=n, fill=n)) +
-  geom_tile()
-  
-    ggplot(aes(x=name, y=value))
-
+youtube %>%   
+  # put themes into one column
+  pivot_longer(funny:use_sex) %>%
+  # select only true rows
+  filter(value==TRUE) %>% 
+  # count number of ads per theme per year
+  group_by(name, year) %>% count() %>% 
+  # Fix theme names
+  mutate(name = str_to_sentence(name), 
+         name = recode(name, "Show_product_quickly"="Shows product quickly", "Use_sex"="Uses sex")) %>% 
+  # start plotting
+  ggplot(aes(x = year, y = name, fill = n)) +
+  # add tiles and change color scheme
+  geom_tile() + scale_fill_viridis_c(option = "E") +
+  # change global theme
+  theme_ipsum() +
+  # edit theme
+  theme(panel.grid.major.y = element_blank(), panel.grid.major.x = element_blank(), 
+        legend.title = element_text(vjust=0.75, family = "Copperplate"),
+        plot.title = element_text(hjust=0.5, size=24, family = "Copperplate"), legend.position = "top") +
   # add those labels
-  labs(
+  labs(title = "Superb Owl ad themes from XXXIV to LIV",
+       x = NULL, y = NULL, fill = "Number of ads",
        caption = "data from FiveThirtyEight via superbowl-ads.com | plot by @rjstevick for #TidyTuesday")
 
 # Saving -------------------------
