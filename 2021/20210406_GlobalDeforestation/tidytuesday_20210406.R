b471469208dc5d978f8afbf4ec8b42a72d36b4e6
Rebecca Stevick <rjstevick@gmail.com>
2021-04-06 13:45:44 +0200

    week 15 edits and comments

--- 2021/20210406_GlobalDeforestation/tidytuesday_20210406.R
+++ 2021/20210406_GlobalDeforestation/tidytuesday_20210406.R
@@ -13,19 +13,24 @@ tuesdata <- tidytuesdayR::tt_load('2021-04-06')
 brazil_loss <- tuesdata$brazil_loss
 
 # Analysis and plotting ----------
-brazil_loss %>% 
-   pivot_longer(commercial_crops:small_scale_clearing) %>% 
-   mutate(nameother = forcats::fct_lump_n(name, w = value, n = 5)) %>% 
-   mutate(nameother = str_to_sentence(str_replace_all(nameother, "_"," "))) %>% 
-   group_by(year, nameother) %>% summarise(sumvalue = sum(value)) %>% 
+brazil_loss %>%
+   # put all causes into long form per year
+   pivot_longer(commercial_crops:small_scale_clearing) %>%
+   # select top 5 causes and assign all others to "Other"
+   mutate(nameother = fct_lump_n(name, w = value, n = 5)) %>%
+   # remove underscores and capitalize
+   mutate(nameother = str_to_sentence(str_replace_all(nameother, "_"," "))) %>%
+   # group all Others into one name and add up the values per year
+   group_by(year, nameother) %>% summarise(sumvalue = sum(value)) %>%
+   # start plotting
    ggplot(aes(x = year, y = sumvalue, fill = reorder(nameother, desc(sumvalue)))) +
    geom_stream(color = "white", alpha = 0.7, type = "ridge", bw = 0.9) +
    geom_stream_label(aes(label = nameother), type = "ridge", family = "Krungthep") +
    scale_fill_manual(values = c("#092215", rev(park_palette("Acadia")))) +
+   scale_x_continuous(breaks = breaks_pretty(), expand = c(0,0)) +
    scale_y_continuous(labels = label_number(suffix = " million ha", scale = 1e-6, accuracy = 1), expand = c(0,0)) +
-   scale_x_continuous(breaks = scales::breaks_pretty(), expand = c(0,0)) +
    theme_minimal() +
-   theme(legend.position = "none", text = element_text(family = "American Typewriter"), 
+   theme(legend.position = "none", text = element_text(family = "American Typewriter"),
          plot.title = element_text(size = 22, family = "Krungthep"), axis.ticks = element_line(size = 0.2),
          panel.grid.minor = element_blank(), panel.grid.major.x = element_blank()) +
    labs(title = "Causes of forest loss in Brazil",
