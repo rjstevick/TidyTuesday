48a86a135cc81d008f8232eeb4f557d50d8a0fcd
Rebecca Stevick <rjstevick@gmail.com>
2021-08-03 11:11:07 +0200

    week 25 web dubois tweets plot and code

--- 2021/20210615_WEBduBois/tidytuesday_20210615.R
+++ 2021/20210615_WEBduBois/tidytuesday_20210615.R
@@ -1,21 +1,35 @@
-# WEB Du Bois challenge tweets
+# WEB Du Bois challenge tweet locations
 # TidyTuesday 2021 week 25
 # Rebecca Stevick updated 6/23/2021
 
 # Load libraries -----------------
 library(tidyverse)
+library(hrbrthemes)
+library(emojifont)
 
 # Load data ----------------------
 tuesdata <- tidytuesdayR::tt_load('2021-06-15')
 tweets <- tuesdata$tweets
 
 # Analysis and plotting ----------
-tweets %>%
-
-
+ggplot() +
+   # creat world map
+   geom_polygon(data=map_data("world"), aes(x = long, y = lat, group = group), 
+                fill = "grey80", color = "gray90", lwd = 0.3) +
+   # add a blue twitter bird per tweet, with white outline
+   geom_text(data=tweets, aes(x = long, y = lat), color = "white", 
+             family = 'fontawesome-webfont', label = fontawesome("fa-twitter"), size = 2.5) +
+   geom_text(data=tweets, aes(x = long, y = lat), color = "dodgerblue", 
+             family = 'fontawesome-webfont', label = fontawesome("fa-twitter"), size = 2) +
+   # set global theme and fix coordinates
+   theme_ft_rc() + coord_fixed(1.3) +
+   # edit theme
+   theme(plot.title = element_text(size=12), 
+         axis.text.x = element_blank(), axis.text.y = element_blank()) +
    # add those labels
-   labs(
-        caption = "data from `#DuBoisChallenge` tweets | plot by @rjstevick for #TidyTuesday")
+   labs(x = NULL, y = NULL, 
+        title = "Locations of tweets for the #DuBoisChallenge in 2021",
+        caption = "data from #DuBoisChallenge tweets | plot by @rjstevick for #TidyTuesday")
 
 # Saving -------------------------
-ggsave("WEBduBois_plot.png", bg = "transparent", width = 12, height = 7, dpi = 400)
+ggsave("WEBduBois_plot.png", bg = "transparent", width = 8, height = 5, dpi = 400)
